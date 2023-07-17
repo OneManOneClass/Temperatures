1a32ed958c926d6733dbc2201c6ec02e3c31acd4
OneManOneClass <engelaitis@gmail.com>
2023-07-18 02:31:33 +0300

    added functionality

--- models/arduino_db.py
+++ models/arduino_db.py
@@ -19,13 +19,31 @@ class Arduino(Base):
 
     forecast = relationship("Forecast", uselist=False, backref="arduino")
 
+    def __str__(self):
+        # print(f"{self.id:<10}"
+        #         f"{self.temp_value:<6}"
+        #         f"{self.temp_date.strftime('%Y-%m-%d %H:%M:%S'):<15}    "
+        #         f"{self.latitude:<10.2f}"
+        #         f"{self.longitude:<10.2f}")
+
+        return (f"{self.id:<10}"
+                f"{self.temp_value:<6}"
+                f"{self.temp_date.strftime('%Y-%m-%d %H:%M:%S'):<15}    "
+                f"{self.latitude:<10.2f}"
+                f"{self.longitude:<10.2f}")
+
 
 class Forecast(Base):
     __tablename__ = 'forecast'
     id = Column(Integer, primary_key=True, autoincrement=True)
     forecast_value = Column(Float)
-    temp_date = Column(DateTime, default=datetime.utcnow)
 
     arduino_id = Column(Integer, ForeignKey('arduino.id'))
+
+    def __str__(self):
+        return (f"{self.id:<10}"
+                f"{self.forecast_value:<10.2f}"
+                f"{self.arduino_id:<10}")
+
 
 Base.metadata.create_all(engine)
